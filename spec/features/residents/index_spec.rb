167b8ef0de8bd28e4680d27520473ac9ffeafb5e
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-29 15:10:45 -0800

    Complete user story 1

---
+++ spec/features/residents/index_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Residents Index' do
+  describe 'As a user when I visit /residents' do
+    before :each do
+      @tim = Resident.create!(name: 'Tim', age: 34, occupation: 'Writer')
+      @bob = Resident.create!(name: 'Bob', age: 45, occupation:'Baker')
+      @mat = Resident.create!(name: 'Mat', age: 22, occupation: 'Musician')
+    end
+    it 'shows all residents and their name, age, and occupation' do
+      visit "/residents"
+
+      within "#resident_#{@tim.id}" do
+        expect(page).to have_content(@tim.name)
+        expect(page).to have_content(@tim.age)
+        expect(page).to have_content(@tim.occupation)
+        expect(page).to_not have_content(@bob.name)
+        expect(page).to_not have_content(@mat.name)
+      end
+
+      within "#resident_#{@bob.id}" do
+        expect(page).to have_content(@bob.name)
+        expect(page).to have_content(@bob.age)
+        expect(page).to have_content(@bob.occupation)
+        expect(page).to_not have_content(@tim.name)
+        expect(page).to_not have_content(@mat.name)
+      end      
+      
+      within "#resident_#{@mat.id}" do
+        expect(page).to have_content(@mat.name)
+        expect(page).to have_content(@mat.age)
+        expect(page).to have_content(@mat.occupation)
+        expect(page).to_not have_content(@tim.name)
+        expect(page).to_not have_content(@bob.name)
+      end      
+    end
+  end
+end
